0bcf569cad9a57eb0c6d8ef1dbcb286cd65fd1c4
unknown <berkkan22@gmail.com>
2019-03-19 19:45:49 +0100

    Updates

---
+++ precache-manifest.22c54d0502db3fcc77a9597799bc71f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d2310e0b7c371c8a3df",
+    "url": "/schach-app-web-firebase/static/js/runtime~main.e6aed361.js"
+  },
+  {
+    "revision": "206cf9cc7165aefa7b38",
+    "url": "/schach-app-web-firebase/static/js/main.fe9be964.chunk.js"
+  },
+  {
+    "revision": "0ad7dbeab628804cf3b9",
+    "url": "/schach-app-web-firebase/static/js/2.ac4622c4.chunk.js"
+  },
+  {
+    "revision": "206cf9cc7165aefa7b38",
+    "url": "/schach-app-web-firebase/static/css/main.aa79ba74.chunk.css"
+  },
+  {
+    "revision": "8f76f5d5cc699e37fb0a8ecee169e638",
+    "url": "/schach-app-web-firebase/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/schach-app-web-firebase/precache-manifest.22c54d0502db3fcc77a9597799bc71f1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/schach-app-web-firebase/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
